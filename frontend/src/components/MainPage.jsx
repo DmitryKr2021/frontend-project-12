f1b89717b1cd29b71d3985675b6c0b622534db4e
Dmitry <krdvmail@mail.ru>
2024-01-08 16:34:12 +0300

    project 4 refactor 33

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -5,7 +5,7 @@ import Channels from './Channels.jsx';
 import Messages from './Messages.jsx';
 
 const MainPage = () => (
-    <>
+  <>
     <ToastContainer />
     <div className="container h-100 my-4 overflow-hidden rounded shadow">
       <div className="row h-100 bg-white flex-md-row">
