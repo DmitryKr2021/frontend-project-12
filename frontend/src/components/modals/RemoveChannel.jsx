1a6bb4009ba7b1318f28cc48f682fb5166302adf
Dmitry <krdvmail@mail.ru>
2024-01-26 17:32:36 +0300

    review refactor 3-4

--- frontend/src/components/modals/RemoveChannel.jsx
+++ frontend/src/components/modals/RemoveChannel.jsx
@@ -5,7 +5,6 @@ import {
   Form,
   Modal,
 } from 'react-bootstrap';
-import { ToastContainer, toast } from 'react-toastify';
 import { useTranslation } from 'react-i18next';
 import store from '../../slices/index';
 import { closeModal } from '../../slices/modals';
@@ -14,62 +13,46 @@ import { ApiContext } from '../contexts/index.jsx';
 const RemoveChannel = (params) => {
   const { dispatch } = store;
   const { channelNumber } = params;
-  const { socket } = useContext(ApiContext).socket;
+  const { withEmit } = useContext(ApiContext);
+  const { emitRemoveChannel } = withEmit;
   const { t } = useTranslation();
 
   const close = () => {
     dispatch(closeModal());
   };
 
-  const setNotify = (text, result) => {
-    const notify = () => toast[result](text);
-    notify();
-  };
-
   const handleRemove = async () => {
-    try {
-      await socket.emit('removeChannel', { id: channelNumber }, (response) => {
-        const { status } = response;
-        if (status === 'ok') {
-          setNotify(t('toasts.channelRemoved'), 'success');
-        }
-      });
-    } catch (error) {
-      setNotify(t('rename.channelNotRemoved'), 'error');
-    }
+    emitRemoveChannel('removeChannel', { id: channelNumber }, t('toasts.channelRemoved'), t('toasts.channelNotRemoved'));
     close();
   };
 
   return (
-    <>
-      <ToastContainer />
-      <div className="fade modal show" tabIndex="-1">
-        <Modal show onHide={close} centered>
-          <Modal.Header closeButton onClick={close}>
-            <Modal.Title>{t('remove.removeChannel')}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form noValidate="" onSubmit={handleRemove}>
-              <div className="d-flex justify-content-end ">
-                <p className="lead w-75 text-start">{t('remove.sure')}</p>
-                <ButtonGroup className="w-50 mt-5">
-                  <Button
-                    type="button"
-                    className="me-2 rounded btn btn-secondary"
-                    onClick={close}
-                  >
-                    {t('remove.cancel')}
-                  </Button>
-                  <Button type="submit" className="rounded btn btn-danger">
-                    {t('remove.remove')}
-                  </Button>
-                </ButtonGroup>
-              </div>
-            </Form>
-          </Modal.Body>
-        </Modal>
-      </div>
-    </>
+    <div className="fade modal show" tabIndex="-1">
+      <Modal show onHide={close} centered>
+        <Modal.Header closeButton onClick={close}>
+          <Modal.Title>{t('remove.removeChannel')}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form noValidate="" onSubmit={handleRemove}>
+            <div className="d-flex justify-content-end ">
+              <p className="lead w-75 text-start">{t('remove.sure')}</p>
+              <ButtonGroup className="w-50 mt-5">
+                <Button
+                  type="button"
+                  className="me-2 rounded btn btn-secondary"
+                  onClick={close}
+                >
+                  {t('remove.cancel')}
+                </Button>
+                <Button type="submit" className="rounded btn btn-danger">
+                  {t('remove.remove')}
+                </Button>
+              </ButtonGroup>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+    </div>
   );
 };
 
