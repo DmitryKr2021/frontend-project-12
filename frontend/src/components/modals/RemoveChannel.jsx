48fb0c4d45f0d67e1783b8ea845210679c407f10
Dmitry <krdvmail@mail.ru>
2024-01-27 18:15:25 +0300

    review refactor 4-1

--- frontend/src/components/modals/RemoveChannel.jsx
+++ frontend/src/components/modals/RemoveChannel.jsx
@@ -7,6 +7,7 @@ import {
   Form,
   Modal,
 } from 'react-bootstrap';
+import { Formik } from 'formik';
 import { useTranslation } from 'react-i18next';
 import store from '../../slices/index';
 import { closeModal } from '../../slices/modals';
@@ -28,16 +29,6 @@ const RemoveChannel = (params) => {
     notify();
   };
 
-  const handleRemove = async () => {
-    try {
-      await emitChannel('removeChannel', { id: channelNumber });
-      setNotify(t('toasts.channelRemoved'), 'success');
-    } catch (err) {
-      setNotify(t('toasts.channelNotRemoved'), 'error');
-    }
-    close();
-  };
-
   return (
     <div className="fade modal show" tabIndex="-1">
       <Modal show={showModal} onHide={close} centered>
@@ -45,23 +36,41 @@ const RemoveChannel = (params) => {
           <Modal.Title>{t('remove.removeChannel')}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form noValidate="" onSubmit={handleRemove}>
-            <div className="d-flex justify-content-end ">
-              <p className="lead w-75 text-start">{t('remove.sure')}</p>
-              <ButtonGroup className="w-50 mt-5">
-                <Button
-                  type="button"
-                  className="me-2 rounded btn btn-secondary"
-                  onClick={close}
-                >
-                  {t('remove.cancel')}
-                </Button>
-                <Button type="submit" className="rounded btn btn-danger">
-                  {t('remove.remove')}
-                </Button>
-              </ButtonGroup>
-            </div>
-          </Form>
+          <Formik
+            initialValues={{
+              id: channelNumber,
+            }}
+            onSubmit={async (values, { setSubmitting }) => {
+              try {
+                await emitChannel('removeChannel', { id: values.id });
+                setNotify(t('toasts.channelRemoved'), 'success');
+              } catch (err) {
+                setNotify(t('toasts.channelNotRemoved'), 'error');
+              }
+              close();
+              setSubmitting(false);
+            }}
+          >
+            {({ handleSubmit }) => (
+              <Form noValidate="" onSubmit={handleSubmit}>
+                <div className="d-flex justify-content-end ">
+                  <p className="lead w-75 text-start">{t('remove.sure')}</p>
+                  <ButtonGroup className="w-50 mt-5">
+                    <Button
+                      type="button"
+                      className="me-2 rounded btn btn-secondary"
+                      onClick={close}
+                    >
+                      {t('remove.cancel')}
+                    </Button>
+                    <Button type="submit" className="rounded btn btn-danger">
+                      {t('remove.remove')}
+                    </Button>
+                  </ButtonGroup>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </Modal.Body>
       </Modal>
     </div>
